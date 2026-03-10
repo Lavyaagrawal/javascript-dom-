814f2e67fada4891866e5f1728cdc6a7e1772178
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-03-10 17:52:59 +0530

    traversing nodes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const banner=document.querySelector('#page-banner')
-console.log('#page-baner node type is:',banner.nodeType)
-console.log("#page-baner node type is:", banner.nodeName);
-console.log("#page-baner has child node:", banner.hasChildNodes());
-const clonedBanner=banner.cloneNode(true);
-console.log(clonedBanner);
+const booklist=document.querySelector('#book-list')
+console.log('the parent node is ',booklist.parentNode)
+console.log("the parent node is ", booklist.parentElement.parentElement);
+console.log(booklist.childNodes);
+console.log(booklist.children)
