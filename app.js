7fb608c2c5efe6d9e720573b9b88d1add0517901
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-03-11 21:23:08 +0530

    traversing the dom(siblings)

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const booklist=document.querySelector('#book-list')
-console.log('the parent node is ',booklist.parentNode)
-console.log("the parent node is ", booklist.parentElement.parentElement);
-console.log(booklist.childNodes);
-console.log(booklist.children)
+const booklist = document.querySelector("#book-list");
+console.log("book-list next siibling is:", booklist.nextSibling);
+console.log("book-list next element siibling is:", booklist.nextElementSibling);
+
+console.log("book-list previous siibling is:", booklist.previousSibling);
+console.log("book-list next previous siibling is:", booklist.previousElementSibling);
+
+booklist.previousElementSibling.querySelector("p").innerHTML+='<br/>TOO COOL FOR EVEYONE ELSE' 
